039d122eab33fb93035bae2519331637cd27bf00
FreshesBrot <schmalz-arthur@web.de>
2019-06-27 15:36:10 +0200

    fixed a little issue with test shader

--- testPxl.hlsl
+++ testPxl.hlsl
@@ -1,3 +1,3 @@
-float4 main() : SV_TARGET {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+float4 main() : SV_TARGET{
+	return float4(0.5f,0.0f,1.0f,1.0f);
 }
--- testVrt.hlsl
+++ testVrt.hlsl
@@ -1,7 +1,8 @@
+
 cbuffer Matrix {
 	matrix m_transform;
 };
 
-float4 main( float3 pos : POSITION, float3 col : color ) : SV_POSITION {
+float4 main(float3 pos : POSITION, float3 col : COLOR) : SV_Position{
 	return mul(m_transform,float4(pos,1.0f));
 }
